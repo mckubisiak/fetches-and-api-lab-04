47c6c4bba548369cd80ee3ce711c452e0009c0ec
Kubi Kubisiak <mckubisiak@gmail.com>
2021-06-10 14:37:51 -0700

    corrected local files

---
+++ src/PokeItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+//take a single pokeman in as a prop
+
+export default class PokeItem extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
---
+++ src/PokeList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+//
+
+export default class PokeList extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
