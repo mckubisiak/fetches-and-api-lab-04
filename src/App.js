028bbbaaae1e4e21eb5e3490316e8c8522839068
Kubi Kubisiak <mckubisiak@gmail.com>
2021-06-10 16:03:51 -0700

    intial components created and api connected dispisplaying intial pokemon page

--- src/App.js
+++ src/App.js
@@ -1,38 +1,35 @@
 import Header from './Header';
+import PokeList from './PokeList';
 import './App.css';
-
+import request from 'superagent';
 import React, { Component } from 'react'
 
+
+
 export default class App extends Component {
+  state = {
+    sortOrder: '',
+    query: '',
+    pokeApi: []
+  }
+
+
+  componentDidMount = async () => {
+  
+    const data = await request.get('https://pokedex-alchemy.herokuapp.com/api/pokedex?pokemon=')
+    this.setState({pokeApi: data.body.results})
+
+    console.log(this.state.pokeApi);
+  }
+  
+
   render() {
     return (
       <div>
         <Header />
+        <PokeList pokemon={this.state.pokeApi} />
+
       </div>
     )
   }
 }
-
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
--- src/PokeList.js
+++ src/PokeList.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react'
+import PokeItem  from './PokeItem'
+
 
 //
 
+
+
 export default class PokeList extends Component {
     render() {
+        let pokedex = this.props.pokemon;
+
+
         return (
-            <div>
+            <div className='pokeman-display'>
                 
+                
+                { pokedex.map((pokemon, i) => <PokeItem data={pokemon}  key={i} />)}
+                
+                
+
+
+
+
             </div>
         )
     }
